a5533ad1869351ea0ca4dc22ea38b201983b9754
keshavsoft <nknnkumar@yahoo.com>
2026-06-17 10:24:30 +0530

    to test

--- bin/v4/AddTableName/Helpers/template/v3/end-points.js
+++ bin/v4/AddTableName/Helpers/template/v3/end-points.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 const tableName = "<TABLE_NAME>";
+const tablePath = "Data/<TABLE_NAME>.json";
+const configPath = "Config/Schema/<TABLE_NAME>.json";
 
 const router = express.Router();
 
